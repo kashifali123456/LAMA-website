e2c6e982b3b65c8ccbac7633962ec35ff102994a
kashif ali <https://github.com/kashifali123456/fyp_project.git>
2023-05-02 22:49:10 +0500

    initial code and Navbar create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  return <>
+  <Home/>
+  {/* <Product/>
+  <ProductList/>
+  <Register/>
+  <Login/>
+  <Cart/> */}
+  </>
 }
 
 export default App;
