e0a2be6d70855982a83282f7d6849712608e706a
kashif ali <https://github.com/kashifali123456/fyp_project.git>
2023-05-03 00:18:52 +0500

    create data file

---
+++ src/data.js
@@ -0,0 +1,90 @@
+import Image1 from "./Assets/Image1.jpg";
+import Image2 from "./Assets/Image2.jpg";
+import Image3 from "./Assets/Image3.jpg";
+import Image4 from "./Assets/Image4.jpg";
+import Image5 from "./Assets/Image5.jpg";
+import Image6 from "./Assets/Image6.jpg";
+import Image7 from "./Assets/image7.jpg";
+import Image8 from "./Assets/images8.jpg";
+import Image9 from "./Assets/images9.jpg";
+import Image10 from "./Assets/images10.jpg";
+import Image11 from "./Assets/images11.jpg";
+import Image12 from "./Assets/images12.jpg";
+import Image13 from "./Assets/images13.jpg";
+import Image14 from "./Assets/images14.jpg";
+export const sliderItems = [
+    {
+      id: 1,
+      img: Image1,
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: Image2,
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: Image3,
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: Image4,
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: Image5,
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: Image6,
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img: Image7,
+    },
+    {
+      id:2,
+      img:Image8,
+    },
+    {
+      id:3,
+      img:Image9,
+    },
+    {
+      id:4,
+      img:Image10,
+    },
+    {
+      id:5,
+      img:Image11,
+    },
+    {
+      id:6,
+      img:Image12,
+    },
+    {
+      id:7,
+      img:Image13,
+    },
+    {
+      id:8,
+      img:Image14,
+    },
+  ]
